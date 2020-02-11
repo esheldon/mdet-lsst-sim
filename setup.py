05cab9bb5c217f84f689913f9d0e109237004bdf
Erin Sheldon <erin.sheldon@gmail.com>
2020-02-11 09:07:24 -0500

    install script

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
+import os
+import glob
 from setuptools import setup, find_packages
+
+scripts = glob.glob('bin/*')
+scripts = [s for s in scripts if '~' not in s]
 
 setup(
     name="mdet_lsst_sim",
     version="0.1.0",
     packages=find_packages(),
+    scripts=scripts,
     # install_requires=['numpy', 'galsim'],
     # include_package_data=True,
     author='Erin Sheldon',
