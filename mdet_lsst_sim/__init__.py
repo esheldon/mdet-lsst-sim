0d06bb834c11a1104fd875fcf00414bc0505dc25
Erin Sheldon <erin.sheldon@gmail.com>
2021-06-10 11:39:06 -0400

    bump version to 0.2.0, read version from package

--- mdet_lsst_sim/__init__.py
+++ mdet_lsst_sim/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-
+from .version import __version__
 from . import run_sim
 from . import util
 from . import vis
--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
+import os
 import glob
 from setuptools import setup, find_packages
+
+__version__ = None
+
+pth = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "mdet_lsst_sim",
+    "version.py"
+)
+with open(pth, 'r') as fp:
+    exec(fp.read())
 
 scripts = glob.glob('bin/*')
 scripts = [s for s in scripts if '~' not in s]
 
 setup(
     name="mdet_lsst_sim",
-    version="0.1.0",
+    version=__version__,
     packages=find_packages(),
     scripts=scripts,
     # install_requires=['numpy', 'galsim'],
