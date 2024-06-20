67efc7e812816afbfa3c783994e58ba0ac519555
Erin Sheldon <erin.sheldon@gmail.com>
2024-06-20 15:50:04 -0400

    updating for newer sims and lsst mdet

--- mdet_lsst_sim/run_sim_phot.py
+++ mdet_lsst_sim/run_sim_phot.py
@@ -2,17 +2,22 @@ import time
 import logging
 import numpy as np
 
-import ngmix
 from descwl_shear_sims.sim import (
     make_sim,
     get_sim_config,
     get_se_dim,
 )
 from descwl_shear_sims.galaxies import make_galaxy_catalog
-from descwl_shear_sims.psfs import make_fixed_psf, make_ps_psf
-from descwl_shear_sims.stars import StarCatalog
-from descwl_coadd import make_coadd_obs, make_coadd_obs_nowarp
+from descwl_shear_sims.psfs import make_fixed_psf, make_ps_psf, make_rand_psf
+from descwl_shear_sims.stars import make_star_catalog
 from metadetect.lsst.photometry import run_photometry
+import metadetect.lsst.vis as lsst_vis
+from metadetect.lsst.masking import (
+    apply_apodized_edge_masks_mbexp,
+    apply_apodized_bright_masks_mbexp,
+    AP_RAD,
+)
+from metadetect.masking import get_ap_range
 import fitsio
 import esutil as eu
 
@@ -64,9 +69,11 @@ def run_sim_phot(
 
     logger.info(f"seed: {seed}")
 
+    ap_padding = get_ap_range(AP_RAD)
+
     rng = np.random.RandomState(seed)
 
-    mdet_config, use_sx = util.get_mdet_config(config=mdet_config)
+    mdet_config, use_sx, trim_pixels = util.get_mdet_config(config=mdet_config)
 
     coadd_config = util.get_coadd_config(config=coadd_config)
     assert not coadd_config['remove_poisson'], (
@@ -77,7 +84,11 @@ def run_sim_phot(
     sim_config = get_sim_config(config=sim_config)
 
     if sim_config['se_dim'] is None:
-        sim_config['se_dim'] = get_se_dim(coadd_dim=sim_config['coadd_dim'])
+        sim_config['se_dim'] = get_se_dim(
+            coadd_dim=sim_config['coadd_dim'],
+            dither=sim_config['dither'],
+            rotate=sim_config['rotate'],
+        )
 
     if sim_config['gal_type'] != 'wldeblend':
         gal_config = sim_config.get('gal_config', None)
@@ -86,13 +97,13 @@ def run_sim_phot(
         sim_config["layout"] = None
         gal_config = None
 
-    if sim_config['stars']:
-        star_config = sim_config.get('star_config', {})
+    star_config = sim_config.get('star_config', None)
 
     logger.info(str(sim_config))
     logger.info(str(mdet_config))
 
     dlist = []
+    infolist = []
 
     for trial in range(ntrial):
         logger.info('-'*70)
@@ -108,18 +119,28 @@ def run_sim_phot(
             gal_config=gal_config,
         )
         if sim_config['stars']:
-            star_catalog = StarCatalog(
+            star_catalog = make_star_catalog(
                 rng=rng,
                 coadd_dim=sim_config['coadd_dim'],
                 buff=sim_config['buff'],
-                density=star_config.get('density'),
+                star_config=star_config,
             )
+            star_density = star_catalog.density
             logger.info('star_density: %g' % star_catalog.density)
         else:
+            star_density = 0
             star_catalog = None
 
         if sim_config['psf_type'] == 'ps':
-            psf = make_ps_psf(rng=rng, dim=sim_config['se_dim'])
+            psf = make_ps_psf(
+                rng=rng,
+                dim=sim_config['se_dim'],
+                variation_factor=sim_config['psf_variation_factor'],
+            )
+        elif sim_config['randomize_psf']:
+            psf = make_rand_psf(
+                psf_type=sim_config["psf_type"], rng=rng,
+            )
         else:
             psf = make_fixed_psf(psf_type=sim_config["psf_type"])
 
@@ -133,6 +154,7 @@ def run_sim_phot(
             g2=0.0,
             psf=psf,
             star_catalog=star_catalog,
+            draw_stars=sim_config['draw_stars'],
             psf_dim=sim_config['psf_dim'],
             dither=sim_config['dither'],
             rotate=sim_config['rotate'],
@@ -149,59 +171,62 @@ def run_sim_phot(
         if show_sim:
             vis.show_sim(sim_data['band_data'])
 
-        coadd_mbobs = ngmix.MultiBandObsList()
-
         tmcoadd0 = time.time()
-        for band, band_exps in sim_data['band_data'].items():
-            obslist = ngmix.ObsList()
+        coadd_data = util.coadd_sim_data(
+            rng=rng, sim_data=sim_data,
+            **coadd_config
+        )
 
-            if coadd_config['nowarp']:
-                if len(band_exps) > 1:
-                    raise ValueError('only one exp allowed for nowarp')
-
-                coadd_obs, exp_info = make_coadd_obs_nowarp(
-                    exp=band_exps[0],
-                    psf_dims=sim_data['psf_dims'],
-                    rng=rng,
-                    remove_poisson=coadd_config['remove_poisson'],
+        if show:
+            if len(coadd_data['mbexp']) >= 3:
+                lsst_vis.show_mbexp(
+                    coadd_data['mbexp'],
+                    stretch=3,
+                    # q=20,
                 )
-
             else:
+                lsst_vis.show_multi_mbexp(coadd_data['mbexp'])
 
-                coadd_obs, exp_info = make_coadd_obs(
-                    exps=band_exps,
-                    coadd_wcs=sim_data['coadd_wcs'],
-                    coadd_bbox=sim_data['coadd_bbox'],
-                    psf_dims=sim_data['psf_dims'],
-                    rng=rng,
-                    remove_poisson=coadd_config['remove_poisson'],
-                )
+        apply_apodized_edge_masks_mbexp(**coadd_data)
+        if len(sim_data['bright_info']) > 0:
+            # Note padding due to apodization, otherwise we get donuts the
+            # radii coming out of the sim code are not super conservative,
+            # just going to the noise level
+            sim_data['bright_info']['radius_pixels'] += ap_padding
+            apply_apodized_bright_masks_mbexp(
+                bright_info=sim_data['bright_info'],
+                **coadd_data
+            )
+            if show:
+                # lsst_vis.show_image_and_mask(coadd_data['mbexp'])
+                lsst_vis.show_multi_mbexp(coadd_data['mbexp'])
 
-            if coadd_obs is None:
-                break
+        mask_frac = util.get_mask_frac(
+            coadd_data['mfrac_mbexp'],
+            trim_pixels=trim_pixels,
+        )
 
-            coadd_obs.meta['band'] = band
-            obslist.append(coadd_obs)
-            coadd_mbobs.append(obslist)
+        info = util.make_info()
+        info['star_density'] = star_density
+        info['mask_frac'] = mask_frac
+        infolist.append(info)
 
         tmcoadd += time.time() - tmcoadd0
 
-        if coadd_obs is None:
+        logger.info('mask_frac: %g' % mask_frac)
+
+        if mask_frac == 1:
             continue
-
-        # if show:
-        #     coadd_obs.show()
-
-        logger.info('mask_frac: %g' % coadd_obs.meta['mask_frac'])
 
         tmmeas0 = time.time()
 
         res = run_photometry(
-            config=mdet_config,
-            mbobs=coadd_mbobs,
-            rng=rng,
-            show=show,
+            rng=rng, config=mdet_config, show=show,
+            mbexp=coadd_data['mbexp'],
+            mfrac_mbexp=coadd_data['mfrac_mbexp'],
+            ormasks=coadd_data['ormasks'],
         )
+
         tmmeas += time.time() - tmmeas0
 
         if res is None:
@@ -211,12 +236,23 @@ def run_sim_phot(
             res=res,
             meas_type=mdet_config['meas_type'],
             star_catalog=star_catalog,
-            meta=coadd_obs.meta,
+            mask_frac=mask_frac,
             full_output=full_output,
         )
 
         if len(comb_data) > 0:
             dlist.append(comb_data)
+            if trim_pixels > 0:
+                good = util.trim_catalog_boundary_strict(
+                    data=comb_data,
+                    dim=sim_config['coadd_dim'],
+                    trim_pixels=trim_pixels,
+                    checks=['l', 'r', 'u', 'd'],
+                    show=show,
+                )
+                comb_data['primary'][good] = True
+            else:
+                comb_data['primary'] = True
 
     tm_seconds = time.time()-tm0
     tm_minutes = tm_seconds/60.0
@@ -227,6 +263,7 @@ def run_sim_phot(
     print('time meas: %g minutes' % (tmmeas / 60))
     print('time per trial: %g seconds' % tm_per_trial)
 
+    info = eu.numpy_util.combine_arrlist(infolist)
     data = eu.numpy_util.combine_arrlist(dlist)
 
     if output is None:
@@ -235,3 +272,4 @@ def run_sim_phot(
         logger.info('writing: %s' % output)
         with fitsio.FITS(output, 'rw', clobber=True) as fits:
             fits.write(data, extname='1p')
+            fits.write(info, extname='info')
