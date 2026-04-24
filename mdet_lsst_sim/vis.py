45a3d88b3eed003e75269c1a1734ae6278f885fd
Erin Sheldon <erin.sheldon@gmail.com>
2026-04-24 10:50:04 -0400

    plotting of sim

--- mdet_lsst_sim/vis.py
+++ mdet_lsst_sim/vis.py
@@ -2,18 +2,74 @@ def show_sim(data):
     """
     show an image
     """
-    from descwl_coadd.vis import show_images, show_2images
-    from espy import images
+    import matplotlib.pyplot as mplt
+    from matplotlib.colors import AsinhNorm
+    import lsst.geom as geom
 
-    imlist = []
+    nim = 0
     for band in data:
         for se_obs in data[band]:
-            sim = se_obs.image.array
-            sim = images.asinh_scale(image=sim / sim.max(), nonlinear=0.14)
-            imlist.append(sim)
-            imlist.append(se_obs.get_psf(25.1, 31.5).array)
+            nim += 1
 
-    if len(imlist) == 2:
-        show_2images(*imlist)
-    else:
-        show_images(imlist)
+    fig, axs = mplt.subplots(
+        layout='tight',
+        nrows=nim,
+        ncols=2,
+        figsize=(9, 4.0 * nim),
+    )
+
+    im_norm = AsinhNorm(
+        linear_width=0.14,
+        clip=False,
+    )
+    psf_norm = AsinhNorm(
+        linear_width=0.01,
+        clip=False,
+    )
+
+    pos = geom.Point2D(25.1, 31.5)
+    im_index = 0
+    for band in data:
+        for epoch, se_obs in enumerate(data[band]):
+
+            seimage = se_obs.image.array
+            seimage = seimage / seimage.max()
+
+            dmpsf = se_obs.getPsf()
+            psf_image = dmpsf.computeImage(pos).array
+            psf_image = psf_image / psf_image.max()
+
+            plot_with_colorbar(
+                fig=fig,
+                ax=axs[im_index, 0],
+                image=seimage,
+                cmap='inferno',
+                norm=im_norm,
+                title=f'band: {band} epoch: {epoch}',
+            )
+            plot_with_colorbar(
+                fig=fig,
+                ax=axs[im_index, 1],
+                image=psf_image,
+                cmap='inferno',
+                norm=psf_norm,
+                title='PSF',
+            )
+
+            im_index += 1
+
+    mplt.show()
+    mplt.close(fig)
+
+
+def plot_with_colorbar(fig, ax, image, title=None, **kw):
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+    pim = ax.imshow(image, **kw)
+
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    fig.colorbar(pim, cax=cax)
+
+    if title is not None:
+        ax.set_title(title)
